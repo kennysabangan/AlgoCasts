cdbd2eb85e21d1f3deac675123b0530293da104c
kennysabangan <61653677+kennysabangan@users.noreply.github.com>
2022-04-10 22:32:31 -0700

    update

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,40 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     let reversed = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+
+// Done with a reversed array
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let char of str) {
+//         reversed = char + reversed;
+//     }
+
+//     return reversed;
+// }
+
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((reversed, char) => (
+        char + reversed
+    ), '')
+}
 
 module.exports = reverse;
--- exercises/reversestring/test.js
+++ exercises/reversestring/test.js
@@ -2,12 +2,12 @@ const reverse = require('./index');
 
 test('Reverse function exists', () => {
   expect(reverse).toBeDefined();
-});
+})
 
 test('Reverse reverses a string', () => {
   expect(reverse('abcd')).toEqual('dcba');
-});
+})
 
 test('Reverse reverses a string', () => {
-  expect(reverse('  abcd')).toEqual('dcba  ');
-});
+  expect(reverse('   abcd')).toEqual('dcba   ')
+})

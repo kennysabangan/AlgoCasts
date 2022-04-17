a8a4bf6b620faac887b6c39ea2de32fb7c577319
kennysabangan <61653677+kennysabangan@users.noreply.github.com>
2022-04-17 10:18:19 -0700

    Weave, Stacks, Queues, qFromS, LinkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,88 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        return this;
+    }
+
+    size() {
+        let length = 0;
+        let node = this.head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node.next) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        let node = this.head;
+        let previous = null;
+
+        // Case: Length of 0
+        if (!this.head) {
+            return;
+        }
+
+        // Case: Length of 1
+        if (!this.head.next) {
+            this.head = null;
+            return;
+        }
+
+        // Case: Length of 2+
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+
+        previous.next = null;
+        return
+    }
+
+    insertLast(data) {
+
+    }
+}
 
 module.exports = { Node, LinkedList };
--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,77 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+// class Queue {
+//     constructor() {
+//         this.queue = [];
+//     }
+
+//     add(val) {
+//         this.queue.unshift(val);
+//     }
+
+//     remove() {
+//         return this.queue.pop();
+//     }
+// }
+
+class Queue {
+    constructor() {
+        this.head = null;
+    }
+
+    add(val) {
+        const node = new Node(val);
+        if (!this.head) {
+            this.head = node;
+            return this;
+        }
+
+        node.next = this.head;
+        this.head = node;
+        return this;
+    }
+
+    remove() {
+        var runner = this.head;
+        var follower = null;
+
+        if (!this.head.next) {
+            const removed = this.head.val;
+            this.head.val = undefined;
+            return removed;
+        }
+
+        while (runner.next != null) {
+            follower = runner;
+            runner = runner.next;
+        }
+
+        follower.next = null;
+        return runner.val;
+    }
+
+    display() {
+        var runner = this.head;
+        var display = "List: ";
+        while (runner != null) {
+            display += runner.val + ' ';
+            runner = runner.next;
+        }
+        console.log(display);
+        return display;
+    }
+}
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const q = new Queue();
+q.add(1).add(2).add(3)
+q.display();
 
 module.exports = Queue;

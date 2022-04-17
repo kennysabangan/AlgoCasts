bcc34526e2c3be529f4c92ce0d608da5f9cb77a8
kennysabangan <61653677+kennysabangan@users.noreply.github.com>
2022-04-16 21:55:49 -0700

    Fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,62 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Iterative Solution
+// function fib(n) {
+//     const fib = [0, 1]
+
+//     for (let i = 2; i <= n; i++) {
+//         fib[i] = fib[i-2] + fib[i-1];
+//     }
+
+//     return fib[n];
+// }
+
+// Recursive Solution
+// function fib(n) {
+//     if (n <= 1) return n;
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// function memoize(fn) {
+//     const cache = {};
+//     return function(...args) {
+//         if (cache[args]) {
+//             return cache[args];
+//         }
+
+//         const result = fn.apply(this, args);
+//         cache[args] = result;
+//         return result;
+//     }
+// }
+
+// fib = memoize(fib);
+
+
+// Do it again! Recursion + Memoization
+function fib(n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    return fib(n - 2) + fib(n - 1);
+}
+
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args] // fib[3] = 2 ? return 2;
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
